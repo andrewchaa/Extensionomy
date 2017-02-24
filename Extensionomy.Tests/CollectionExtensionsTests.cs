121c191013edcf22ef004834d49e7bd1bd8983bb
andrewchaa <andrew.chaa@gsacapital.com>
2017-02-24 09:25:43 +0000

    first commit

---
+++ Extensionomy.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Machine.Specifications;
+
+namespace Extensionomy.Tests
+{
+    [Subject(typeof(CollectionExtensions))]
+    public class CollectionExtensionsTests
+    {
+        private static IList<string> _collection;
+
+        Establish context = () =>
+        {
+            _collection = new List<string>();
+        };
+
+        It should_be_empty = () => _collection.IsEmpty().ShouldBeTrue();
+    };
+}
